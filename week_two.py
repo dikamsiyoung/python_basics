7bde33d34753c5c7d4ad3266e48cb178769787d1
TheKamsiYoung <47384524+TheKamsiYoung@users.noreply.github.com>
2020-09-06 00:15:02 +0100

    Added Week Two Python

---
+++ week_two.py
@@ -0,0 +1,21 @@
+#  Sequences
+a_string = 'is an immutable sequence'
+a_list = ['is', 'a', 'mutable', 'sequence', 'and']
+a_tuple = ('is', 'a', 'sequence', 'but', 'immutable')
+
+
+# Iterations
+to_iterate = 'within a sting use a for loop'
+iterate_words = 'in a string as well by spliting it using'
+the_split_command = iterate_words.split(' ')
+
+for char in to_iterate:
+    print(char)
+
+for word in the_split_command:
+    print(word)
+
+count_items_using_accumulator = 0
+
+for word in range(len(a_tuple)):
+    count_items_using_accumulator += 1
